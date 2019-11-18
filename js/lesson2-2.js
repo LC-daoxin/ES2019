10b139d0fd13681230f4d82cea0a3a63edc49071
LC-daoxin <457121827@qq.com>
2019-11-19 00:49:14 +0800

    update Array

--- js/lesson2-2.js
+++ js/lesson2-2.js
@@ -1,1 +1,33 @@
 /* Array */
+// for 循环
+const arr = [1, 2, 3, 4, 5]
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i])
+}
+
+// forEach
+arr.forEach(function (item) {
+  console.log(item)
+})
+
+// arr.forEach(callback[, thisArg])
+
+// every
+
+arr.every(function (item) {
+  console.log(item)
+  return true
+})
+
+// for in
+for (let index in arr) {
+  console.log(arr[index])
+}
+
+// map
+
+arr.map(function (item) {
+  console.log(item)
+})
+
+// for of
