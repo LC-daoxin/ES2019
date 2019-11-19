4ece01db49f74b9bb85e2ff0825d35a76a7ba21e
LC-daoxin <457121827@qq.com>
2019-11-19 16:35:48 +0800

    update Class

--- js/lesson2-2.js
+++ js/lesson2-2.js
@@ -31,3 +31,65 @@ arr.map(function (item) {
 })
 
 // for of
+
+const Price = {
+  A: [1, 2, 3],
+  B: [0.1, 0.3, 0.5],
+  C: [0.2, 4, 13]
+}
+
+for (let key of Object.keys(Price)) {
+  console.log(Price[key])
+}
+
+Array.prototype.method = function () {
+  console.log(this.length)
+}
+const myArray = [1, 2, 4, 5, 6, 7]
+myArray.name = '数组'
+for (let value of myArray) {
+  console.log(value)
+}
+for (let key in myArray) {
+  console.log(key)
+}
+list(2, 3, 4)
+function list (arr) {
+  // console.log(arguments.slice(1))
+  // for (let key of arguments) {
+  //   console.log(key)
+  // }
+  console.log(Array.prototype.slice.call(arguments))
+  let NewArr = Array.from(arguments, function multiply (item) {
+    return item * 2
+  })
+  console.log(NewArr)
+}
+let arraylike = Array(5)
+let array = Array.from(arraylike, function (item) {
+  return 1
+})
+console.log(array)
+console.log(array.slice(1))
+
+// Array.of
+let arrof = Array.of(1, 3, 4)
+console.log(arrof)
+
+// Array.fill()
+let One = Array(5).fill(2)
+console.log(One)
+
+// Array.filter
+let Two = [2, 5, 7, 11, 22]
+let find = Two.filter(function (item) {
+  return item > 6
+})
+console.log(find)
+
+// Array.find
+let Two2 = [2, 5, 7, 11, 22]
+let find2 = Two2.find(function (item) {
+  return item > 6
+})
+console.log(find2)
