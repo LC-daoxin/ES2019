55d3543d135b6a11ec58179a75e8328b60da974c
LC-daoxin <457121827@qq.com>
2019-11-27 15:42:13 +0800

    add Class

--- js/lesson2-3.js
+++ js/lesson2-3.js
@@ -1,1 +1,24 @@
 /* Class */
+let _age = 4
+class Animal {
+  constructor (type) {
+    this.type = type
+  }
+  get age () {
+    return _age
+  }
+  set age (value) {
+    _age = value
+  }
+  eat () {
+    console.log('i an eat food')
+  }
+}
+
+let dog = new Animal('dog')
+let monkey = new Animal('monkey')
+dog.age = 5
+const { eat } = dog
+eat()
+console.log(dog)
+console.log(monkey)
