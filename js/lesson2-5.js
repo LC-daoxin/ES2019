abd195e8f0a4d91df23f6604e7144a930455dbbe
LC-daoxin <457121827@qq.com>
2019-12-05 10:33:20 +0800

    update

--- js/lesson2-5.js
+++ js/lesson2-5.js
@@ -6,3 +6,13 @@ s.add('hello').add('world')
 // s.clear()
 console.log(s.has('hello'))
 console.log(s.size)
+
+function Timer () {
+  this.S1 = 0
+  // setInterval(function () {
+  //   this.S1++
+  // }, 1000)
+  setInterval(() => this.S1++, 1000)
+}
+let timer = new Timer()
+setTimeout(() => console.log(timer.S1), 3100)
