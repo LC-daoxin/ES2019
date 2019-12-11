9f317fe32edf4fa3b9d41c82cabb6b7d45292a5e
LC-daoxin <457121827@qq.com>
2019-12-11 11:27:47 +0800

    update

--- js/lesson2-7.js
+++ js/lesson2-7.js
@@ -23,7 +23,51 @@ console.log(dog.age)
 
 /* Proxy */
 let o = {
-  name: 'text'
+  age: 19
 }
-let obj = new Proxy(o, {})
+let obj = new Proxy(o, {
+  get (target, key, receiver) {
+    console.log(receiver)
+    if (key === 'age') {
+      return target[key] + 20
+    } else {
+      return 0
+    }
+  },
+  set (target, key, value) {
+    return false
+  }
+})
 console.log(Reflect.getPrototypeOf(obj))
+console.log(obj.age)
+obj.abc = 123
+console.log(obj)
+console.log(o)
+
+let a = {
+  name: 'xiaomong',
+  price: 200
+}
+let handler =  {
+  get (target, key) {
+    return target[key] || ''
+  },
+  set (target, key, value) {
+    if (Reflect.has(target, key)) {
+      if (key === 'price') {
+        if (value > 300) {
+          return false
+        } else {
+          target[key] = value
+        }
+      } else {
+        target[key] = value
+      }
+    } else {
+      return false
+    }
+  }
+}
+let d = new Proxy(a, handler)
+d.price = 302
+console.log(d)
